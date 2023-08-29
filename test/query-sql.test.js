d9f078e1f8c43bfa0223cc717f25a798e2624ba3
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-08-29 10:54:50 +0700

    Query SQL

---
+++ test/query-sql.test.js
@@ -0,0 +1,15 @@
+import {prismaClient} from "../src/prisma-client.js";
+
+describe("Prisma Client", () => {
+    it('should be able to execute sql', async () => {
+        const id = "1";
+
+        const samples = await prismaClient.$queryRaw`SELECT *
+                                                     FROM sample
+                                                     WHERE id = ${id}`
+
+        for (const sample of samples) {
+            console.log(`Result sample id: ${sample.id} and name: ${sample.name}`)
+        }
+    });
+});

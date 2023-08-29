79dcf19b08fb4f5ab1c739f4ae37349f3914ffe3
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-08-29 09:55:09 +0700

    Prisma Client test

--- test/prisma-client.test.js
+++ test/prisma-client.test.js
@@ -1,12 +1,12 @@
-import {PrismaClient} from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
-describe("Prisma Client", () => {
-    it('should be able to connect to database', async () => {
-        const prisma = new PrismaClient();
-        await prisma.$connect();
+describe('Prisma Client', () => {
+  it('should be able to connect to database', async () => {
+    const prisma = new PrismaClient();
+    await prisma.$connect();
 
-        // do something
+    // do something
 
-        await prisma.$disconnect();
-    });
+    await prisma.$disconnect();
+  });
 });

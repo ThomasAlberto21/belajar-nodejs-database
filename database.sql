9b46c43c44c41758436747a7498f56287f034908
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-08-30 10:26:17 +0700

    Create Table Customers

--- database.sql
+++ database.sql
@@ -1,7 +1,22 @@
-CREATE TABLE sample(
-    id VARCHAR(100) NOT NULL ,
-    name VARCHAR(100) NOT NULL ,
+CREATE TABLE sample
+(
+    id   VARCHAR(100) NOT NULL,
+    name VARCHAR(100) NOT NULL,
     PRIMARY KEY (id)
 ) ENGINE Innodb;
 
-SELECT * FROM sample;
+
+
+CREATE TABLE customers
+(
+    id    VARCHAR(100) NOT NULL,
+    name  VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    phone VARCHAR(100) NOT NULL,
+    CONSTRAINT customers_email_unique UNIQUE (email),
+    CONSTRAINT customers_phone_unique UNIQUE (phone),
+    PRIMARY KEY (id)
+) ENGINE Innodb;
+
+
+

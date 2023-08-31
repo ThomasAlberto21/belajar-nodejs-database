484806a3beb8e567dfd00eaac103a2fae9a2996a
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-08-31 10:15:50 +0700

    Buat database products

--- database.sql
+++ database.sql
@@ -22,3 +22,22 @@ CREATE TABLE customers
 SELECT *
 FROM customers;
 
+CREATE TABLE products
+(
+    id          VARCHAR(100) NOT NULL,
+    name        VARCHAR(100) NOT NULL,
+    stock       INT          NOT NULL,
+    price       INT          NOT NULL,
+    category_id VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE Innodb;
+
+INSERT INTO products (id, name, stock, price, category_id)
+VALUES ('P0001', 'Buku', 1000, 10, 'K1'),
+       ('P0002', 'Pulpen', 1200, 10, 'K1'),
+       ('P0003', 'Pensil', 2000, 10, 'K1'),
+       ('P0004', 'Tas', 10000, 10, 'K1'),
+       ('P0005', 'Kertas', 500, 10, 'K1')
+
+
+SELECT * FROM products
